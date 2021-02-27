aa598a3aad704dffef6db11fccc1c60fbb008aff
IVProb <jnoojinjr@gmail.com>
2021-02-26 18:40:42 -0600

    initial setup up

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // conver the below code to include examples of
+            // a. inferred typing
+            // b. String Interpolation
+            // c. Ternary Operator
+
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+        }
+    }
+}
